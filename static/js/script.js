d9b4de5b7451dbdcd155318d2286ca1f2317b638
Md. Saidul Hoque Anik <onix.hoque@gmail.com>
2021-05-17 21:08:58 +0600

    Synced

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,3 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-    .register('./service-worker.js')
-    .then(function(registration) {
-        console.log('Service Worker Registered!');
-        return registration;
-    })
-    .catch(function(err) {
-        console.error('Unable to register service worker.', err);
-    });
-}
-
 const pages = {
 	"VERSE_DETAILS" : 1,
 	"COMP_ARABIC" : 2,
